d36d288e0b393797a3dae71181d79d9885336930
Desti <remo.koellisch@gmail.com>
2026-04-13 01:35:53 +0200

    java logic update

--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
-function showUebungErstellenUI() {
+console.log("JS geladen");
+window.showUebungErstellenUI = function () {
   const viewCreate = document.getElementById("view-create");
+  const armeaddbox = document.getElementById("armeaddbox");
+
+  armeaddbox.style.display = "none";
+
   viewCreate.innerHTML = `
-    <div class="create-box">
 
-      <h2 class="t2">Übung erstellen</h2>
+    <div class="Createbox">
+    <h2 class="t2">Übung Hinzufügen</h2>
+  </div>
 
-      <input
+  <div class="Createbox">
+    <hr class="Createline">
+  </div>
+
+  <div class="Createbox">
+  <input
         id="uebungName"
         type="text"
         placeholder="Name der Übung"
-        class="input"
+        class="Uebunginput"
       >
+</div>
 
-      <button onclick="speichernUebung()" class="btn">
-        Speichern
-      </button>
+    <div class="Createbox">
+
+      <img src="../../Images/Plus.png" alt="Image not found"  class="image">
+      <h3 class="t3">Übung Speichern</h3>
+
 
     </div>
   `;
