b01876a0a7d7673e3d3adaffb2bb50a88606b96f
Desti <remo.koellisch@gmail.com>
2026-04-18 19:02:55 +0200

    java structure update and table update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,88 @@ document.addEventListener("DOMContentLoaded", function() {
   initalload(); }
 );
 
+window.showExercise = function() {
+  const liste = document.getElementById("liste");
+  const category = document.body.dataset.category;
+  let counter = 0;
+  liste.innerHTML = "";
+
+  allExercises
+    .filter(u => u.category === category)
+    .forEach(u => {
+      const div = document.createElement("div");
+      counter++;
+      console.log(counter);
+
+      div.classList.add("Taskbox");
+
+      div.innerHTML = `
+      <label class="Checkmark">
+        <input type="checkbox">
+        <span></span>
+      </label>
+      <span class="t1">${u.name}</span>
+
+        <button onclick="addRow(${counter})">Zeile hinzufügen</button>
+
+        <table id="Table${counter}" border="1">
+          <thead>
+            <tr id="headerRow${counter}">
+              <th>Spalte 1</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td contenteditable="true"></td>
+            </tr>
+          </tbody>
+        </table>
+    `;
+
+
+
+      liste.appendChild(div);
+    });
+}
+
+window.clearlist = function() {
+  allExercises = [];
+  localStorage.removeItem("uebungen");
+  console.log("list cleared");
+  showExercise();
+};
+
+window.save = function () {
+  const input = document.getElementById("uebungName");
+  const name = input.value;
+  const category = document.body.dataset.category;
+
+  if (name.trim() === "") return;
+
+  allExercises.push({
+    name: name,
+    category: category
+  });
+
+  localStorage.setItem("uebungen", JSON.stringify(allExercises));
+
+  input.value = "";
+  showExercise();
+}
+
+window.loadExercise = function() {
+  const daten = localStorage.getItem("uebungen");
+
+  if(daten) {
+    allExercises = JSON.parse(daten);
+  }
+}
+
+window.initalload = function(){
+  loadExercise();
+  showExercise();
+}
+
 window.showUebungErstellenUI = function () {
   const viewCreatebox = document.getElementById("view-createbox");
   const armeaddbox = document.getElementById("armeaddbox");
@@ -46,99 +128,16 @@ window.showUebungErstellenUI = function () {
   showExercise();
 };
 
-window.save = function () {
-  const input = document.getElementById("uebungName");
-  const name = input.value;
-  const category = document.body.dataset.category;
 
-  if (name.trim() === "") return;
-
-  allExercises.push({
-    name: name,
-    category: category
-  });
-
-  localStorage.setItem("uebungen", JSON.stringify(allExercises));
-
-  input.value = "";
-
-  showExercise();
-}
-
-window.showExercise = function() {
-  const liste = document.getElementById("liste");
-  const category = document.body.dataset.category;
-
-  liste.innerHTML = "";
-
-  allExercises
-    .filter(u => u.category === category)
-    .forEach(u => {
-      const div = document.createElement("div");
-      let counter = 0;
-      div.classList.add("Taskbox");
-
-      div.innerHTML = `
-      <label class="Checkmark">
-        <input type="checkbox">
-        <span></span>
-      </label>
-      <span class="t1">${u.name}</span>
-      <button onclick="addColumn()">Spalte hinzufügen</button>
-
-        <button onclick="addRow()">Zeile hinzufügen</button>
-
-        <table id="myTable" border="1">
-          <thead>
-            <tr id="headerRow">
-              <th>Spalte 1</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td contenteditable="true"></td>
-            </tr>
-          </tbody>
-        </table>
-    `;
-      counter++;
-      print(counter);
-
-      liste.appendChild(div);
-    });
-}
-
-window.loadExercise = function() {
-  const daten = localStorage.getItem("uebungen");
-
-  if(daten) {
-    allExercises = JSON.parse(daten);
-  }
-}
-
-window.clearlist = function() {
-  allExercises = [];
-  localStorage.removeItem("uebungen");
-  console.log("Alles gelöscht");
-  showExercise();
-};
-
-window.initalload = function(){
-  loadExercise();
-  showExercise();
-}
-
-function addRow() {
-  const table = document.getElementById("myTable");
-  const headerCells = document.getElementById("headerRow").children;
-
+function addRow(counter) {
+  const headerCells = document.getElementById(`headerRow${counter}`);
   const tr = document.createElement("tr");
 
-  for (let i = 0; i < headerCells.length; i++) {
-    const td = document.createElement("td");
-    td.contentEditable = "true";
-    tr.appendChild(td);
-  }
+  const table = document.getElementById(`Table${counter}`);
 
-  table.appendChild(tr);
+  const td = document.createElement("td");
+    td.contentEditable = "true";
+
+    tr.appendChild(td);
+    table.appendChild(tr);
 }
